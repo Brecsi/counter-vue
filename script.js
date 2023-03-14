a570fcd322aa08a487009b1be765d0317f8c8a66
Brecsi <lastmans07@gmail.com>
2023-03-14 09:19:31 +0100

    done

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const {createApp } = Vue;
-
-createApp({
-    data(){
-        return{
-            
-        };
+new Vue({
+    el: '#app',
+    data: {
+      count: 0,
+      direction: 1,
     },
-}).mount("#app");
+    methods: {
+      increment: function() {
+        this.count += this.direction;
+      },
+      toggleDirection: function() {
+        this.direction *= -1;
+      },
+    },
+    computed: {
+      incrementButtonText: function() {
+        return this.direction > 0 ? 'Add' : 'Subtract';
+      },
+      directionButtonText: function() {
+        return this.direction > 0 ? 'Forward' : 'Backward';
+      },
+    },
+  });
